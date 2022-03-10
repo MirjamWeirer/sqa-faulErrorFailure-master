44990666f6d5538764c9b2e747fd69a391df3b83
MirjamWeirer <mirjam.weirer@gmx.at>
2022-03-10 15:09:59 +0100

    added zero at start

--- src/at/campus02/example/TestFaultOne.java
+++ src/at/campus02/example/TestFaultOne.java
@@ -4,16 +4,31 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestFaultOne {
+
     @Test
     public void zeroAtEnd(){
-        //Fault (Bug) vorhanden,Error vorhanden, Failure: nein
+        //Fault (Bug) vorhanden
+        //Error vorhanden: i startet bei 1, nicht bei 0
+        //Failure: nein (weil Resultat troztdem,stimmt)
        int res = FaultOne.numZero(new int []{1,2,3,0});
         Assert.assertEquals(1,res);
     }
 
+    @Test
+    public void zeroAtStart(){
+        //Fault (Bug) vorhanden
+        //Error vorhanden: i startet bei 1, nicht bei 0
+        //Failure: nein (weil Resultat troztdem, stimmt)
+        int res = FaultOne.numZero(new int []{0,1,2,3});
+        Assert.assertEquals(0,res);
+    }
+
     @Test(expected = NullPointerException.class)
     public void expectException(){
-        //Fault (Bug) vorhanden,Error vorhanden, Failure: nein
+        //Fault (Bug) vorhanden
+        //Error nicht vorhanden: Exception wird vorher ausgelöst(mit if abfrage)
+        //Failure: nein
         int res = FaultOne.numZero(null);
+        //we never reach this line
     }
 }
