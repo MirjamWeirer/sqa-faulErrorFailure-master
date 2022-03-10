7dcb1d01907f520a0353b01fc2cb85bb63244337
MirjamWeirer <mirjam.weirer@gmx.at>
2022-03-10 15:01:36 +0100

    added Test expectException;

--- src/at/campus02/example/TestFaultOne.java
+++ src/at/campus02/example/TestFaultOne.java
@@ -10,4 +10,10 @@ public class TestFaultOne {
        int res = FaultOne.numZero(new int []{1,2,3,0});
         Assert.assertEquals(1,res);
     }
+
+    @Test(expected = NullPointerException.class)
+    public void expectException(){
+        //Fault (Bug) vorhanden,Error vorhanden, Failure: nein
+        int res = FaultOne.numZero(null);
+    }
 }
