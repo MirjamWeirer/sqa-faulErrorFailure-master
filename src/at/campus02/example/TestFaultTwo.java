febecb01f4ec68bb0c13649d7daa4d83f827f288
MirjamWeirer <mirjam.weirer@gmx.at>
2022-03-10 15:30:02 +0100

    added test nullpointer exception

--- src/at/campus02/example/TestFaultTwo.java
+++ src/at/campus02/example/TestFaultTwo.java
@@ -7,7 +7,13 @@ public class TestFaultTwo {
     @Test
     public void findNumber() {
         int pos = FaultTwo.findLast(new int[] {2, 3, 5, 8}, 2);
-        Assert.assertEquals(1, pos);
+        Assert.assertEquals(0, pos);
+    }
+
+    @Test
+    public void findNumberAtRightPosition(){
+        int pos = FaultTwo.findLast(new int[] {0, 2, 3, 5, 8}, 3);
+        Assert.assertEquals(2,pos);
     }
 
     @Test(expected = NullPointerException.class)
